75b84d67069e2d3fbb446d549752d2ce5ead21f5
SHUBHAM KUMAR SINGH <98935875+ShubhamKumarSingh123@users.noreply.github.com>
2024-02-23 11:39:20 +0530

    Add files via upload
    Added backend of the project

---
+++ api/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import userRouter from "./Routes/user.route.js";
+import authRouter from "./Routes/auth.route.js";
+import listingRouter from "./Routes/listing.route.js";
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+// middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
